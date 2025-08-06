c4fd62b0598d44f30a6ae46094875f1f46c105d1
Lokesh Tomar <lokeshvineettomar@gmail.com>
2025-08-07 03:21:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -103,9 +103,9 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
 
- app.all("*",(req,res,next)=>{
-        next(new ExpressError(404, "Page not found!"));
-   }); 
+ //app.all("*",(req,res,next)=>{
+   //     next(new ExpressError(404, "Page not found!"));
+   //}); 
 
 app.use((err, req, res,  next) =>{
     let {statuscode=500, message="something went wrong !"} = err;
